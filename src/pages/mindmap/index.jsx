7bbf2632820697118955942889ffa0a6b31afc3d
nvvinhb1805939 <nvvinh.dev@gmail.com>
2023-06-12 08:14:10 +0700

    init project

---
+++ src/pages/mindmap/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MindMapPage = (props) => {
+  return <div>mind map</div>;
+};
+
+MindMapPage.propTypes = {};
+
+export default MindMapPage;
